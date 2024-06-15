ebdfad049798e8f5df743cd1f144035e0aa00073
Akmall <akmalmaulanabasri@smkwikrama.sch.id>
2024-06-16 00:19:34 +0700

    downgrade to laravel 10

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::resource('/dashboard', DashboardController::class);
+Route::get('/', function () {
+    return view('dashboard.index');
+});
